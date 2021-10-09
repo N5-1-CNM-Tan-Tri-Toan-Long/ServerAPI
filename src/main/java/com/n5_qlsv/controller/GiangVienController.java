fde0f51c9fe93c7e74fa354d41891670c552ae05
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-10-09 11:56:31 +0700

    Fix GiangVienController

--- src/main/java/com/n5_qlsv/controller/GiangVienController.java
+++ src/main/java/com/n5_qlsv/controller/GiangVienController.java
@@ -30,8 +30,7 @@ public class GiangVienController {
     }
 
     @PutMapping("/{id}")
-    public GiangVien updateGiangVien(@RequestBody GiangVien giangVien,
-                                     @PathVariable ("id") Long id){
+    public GiangVien updateGiangVien(@RequestBody GiangVien giangVien, @PathVariable ("id") Long id){
         return giangVienService.updateGiangVienById(id, giangVien);
     }
 
