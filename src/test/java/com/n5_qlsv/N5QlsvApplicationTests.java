4dd287626368e29d59d62339356c1d15cf0470e1
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-10-28 08:58:56 +0700

    sửa lại sinh viên

--- src/test/java/com/n5_qlsv/N5QlsvApplicationTests.java
+++ src/test/java/com/n5_qlsv/N5QlsvApplicationTests.java
@@ -1,24 +1,13 @@
 package com.n5_qlsv;
 
-import com.n5_qlsv.entity.SinhVien;
-import com.n5_qlsv.service.SinhVienService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class N5QlsvApplicationTests {
-//
-//    @Autowired
-//    private SinhVienService sinhVienService;
 
     @Test
     void contextLoads() {
-//        SinhVien sinhVien = SinhVien.builder()
-//                .password("password")
-//                .roleName("ROLE_ADMIN")
-//                .build();
-//        sinhVienService.saveSinhVien(sinhVien);
-    }
 
+    }
 }
