770e50cc50cce4b8db4c6b8e98de709208ee8c3a
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-11-30 23:23:21 +0700

    Thêm học phần khung

--- src/main/java/com/n5_qlsv/service/HocPhanKhungService.java
+++ src/main/java/com/n5_qlsv/service/HocPhanKhungService.java
@@ -8,10 +8,17 @@ import java.util.List;
 public interface HocPhanKhungService {
 
     HocPhanKhung saveHPK(HocPhanKhung hpk);
+
     HocPhanKhung findHPKById(Long maHPK);
+
     List<HocPhanKhung> findAllHPK(int page, int size);
+
     void deleteHPKById(Long maHPK);
+
     HocPhanKhung updateHPKById(Long maHPK, HocPhanKhung hpk);
+
     void saveHPKByFile(MultipartFile file);
+
+    List<HocPhanKhung> findAllByChuyenNganhAndHocKi(long maCN, int tthk);
 
 }
