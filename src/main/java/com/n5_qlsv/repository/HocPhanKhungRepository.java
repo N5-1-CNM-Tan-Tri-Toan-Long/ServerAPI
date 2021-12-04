9f4842bf0cf875cbc303536bfecca84d3b39f06c
Development <70835318+Development707@users.noreply.github.com>
2021-12-04 09:39:27 +0700

    fix select hoc phan khung

--- src/main/java/com/n5_qlsv/repository/HocPhanKhungRepository.java
+++ src/main/java/com/n5_qlsv/repository/HocPhanKhungRepository.java
@@ -9,12 +9,9 @@ import java.util.List;
 
 @Repository
 public interface HocPhanKhungRepository extends JpaRepository<HocPhanKhung, Long> {
-    @Query(value = "SELECT mahpk, mh.ten_mon_hoc, hpk.ma_hp, (hp.so_tc_lt + hp.so_tc_th) as so_tc, " +
-            "hpk.so_tietlt, hpk.so_tietth, hpk.thu_tu_hoc_ky, hpk.trang_thai  " +
-            "FROM hotrosinhvien.tbl_hocphankhung hpk " +
-            "join hotrosinhvien.tbl_hocphan hp on hp.ma_hp = hpk.ma_hp " +
-            "join hotrosinhvien.tbl_monhoc mh on mh.ma_mh = hp.ma_mh " +
-            "where hp.ma_chuyen_nganh = ?1 and hpk.thu_tu_hoc_ky = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM hotrosinhvien.tbl_hocphankhung k " +
+            "JOIN hotrosinhvien.tbl_hocphan h ON k.ma_hp = h.ma_hp " +
+            "where h.ma_chuyen_nganh = ?1 and k.thu_tu_hoc_ky = ?2", nativeQuery = true)
     List<HocPhanKhung> findAllByChuyenNganhAndHocKi(long maCN, int tthk);
 
     @Query(value = "SELECT * FROM hotrosinhvien.tbl_hocphankhung k " +
